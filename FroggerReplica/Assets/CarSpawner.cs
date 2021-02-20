7a7e735b604e4643045618ebd24aee688973cc0c
fined-nsu <71229695+fined-nsu@users.noreply.github.com>
2021-02-20 11:57:04 -0600

    Tutorial Complete
    Submitting completed tutorial

---
+++ FroggerReplica/Assets/CarSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public float spawnDelay = .3f;
+
+    public GameObject car;
+
+    public Transform[] spawnPoints;
+
+    float nexTimeToSpawn = 0f;
+
+    void Update()
+    {
+        if (nexTimeToSpawn <= Time.time)
+        {
+            SpawnCar();
+            nexTimeToSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnCar()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[randomIndex];
+        
+        Instantiate(car, spawnPoint.position, spawnPoint.rotation);
+    }
+}
